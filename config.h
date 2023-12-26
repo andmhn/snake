4a5346d1c064f2746e2dfb025539e9e31460e067
Anand Mohan <andmhn@outlook.com>
2023-12-26 19:59:39 +0530

    update entity size

--- config.h
+++ config.h
@@ -3,11 +3,16 @@
 
 #include<string>
 
+#define FPS 60
+
 #define WINDOW_HEIGHT  600
 #define WINDOW_WIDTH  600
 
 // width of entity/ sprite in window to render
-#define ENTITY_SIDE  20
+#define ENTITY_SIDE 16
+#define ENTITY_SCALE 2
+
+#define ENTITY_SIZE ENTITY_SIDE * ENTITY_SCALE
 
 // basename of textures
 #endif // CONFIG_H
